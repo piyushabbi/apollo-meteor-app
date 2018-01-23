33e088bfc5b575e65b897a30644958bc1bfd501d
Piyush Abbi <piyushabbi27@gmail.com>
2018-01-23 13:01:35 +0530

    Connecting react to apollo

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,6 +1,25 @@
 import { createApolloServer } from 'meteor/apollo';
 import { makeExecutableSchema } from 'graphql-tools';
 
-createApolloServer({
-  
+// Type Definations
+const typeDefs = `
+  type Query {
+    hi: String
+  }
+`;
+
+// Resolver Obj
+const resolvers = {
+  Query: {
+    hi() {
+      return "Hello Piyush!";
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
 });
+
+createApolloServer({schema});
