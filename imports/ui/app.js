daac933e0ed63baf67da4e0911f4b54423df5411
Piyush Abbi <piyushabbi27@gmail.com>
2018-01-23 13:10:53 +0530

    QraphQl queries in React

--- imports/ui/app.js
+++ imports/ui/app.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
-const App = () => <h1>Hello!</h1>;
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-export default App;
+const App = ({data}) => <h1>{ data.hi }</h1>;
+
+const hiQuery = gql`
+  {
+    hi
+  }
+`;
+
+export default graphql(
+  hiQuery
+)(App);
